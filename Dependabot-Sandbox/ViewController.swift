7698bb0d4e7286652b9e553eab4ca19ebb23e367
Evan Anger <evan_anger@progressive.com>
2020-07-17 13:08:37 -0400

    Initial Commit

---
+++ Dependabot-Sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dependabot-Sandbox
+//
+//  Created by Evan Anger on 7/17/20.
+//  Copyright © 2020 Progressive Insurance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dependabot-SandboxTests/Dependabot_SandboxTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dependabot_SandboxTests.swift
+//  Dependabot-SandboxTests
+//
+//  Created by Evan Anger on 7/17/20.
+//  Copyright © 2020 Progressive Insurance. All rights reserved.
+//
+
+import XCTest
+@testable import Dependabot_Sandbox
+
+class Dependabot_SandboxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
